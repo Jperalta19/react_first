13841c5117400329b9563d2023a735607f20e8d1
Your Name <you@example.com>
2023-01-27 20:53:30 -0400

    .

--- ejercicio sesion 1 2 y 3/src/components/contactos.jsx
+++ ejercicio sesion 1 2 y 3/src/components/contactos.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Contactos} from '../Models/contactos.class';
+import {Contactos, } from '../Models/contactos.class';
 
-const Contactoscomponets = ({contactos}) => {
+function Contactoscomponets  ({contactos})  {
+
+    const [conectado, setConectado] = useState(contactos.estado);
+
     return (
         <div>
-        <h2>Nombre: { contactos.nombre } </h2>
-        <h3>apellido: { contactos.apellido } </h3>
-        <h4>Emali: { contactos.email } </h4>
-        <h5>Estado: { contactos.conectado ? 'Conectado':'Desconectado' } </h5>
+            <h2>Nombre: { contactos.nombre } </h2>
+            <h3>apellido: { contactos.apellido } </h3>
+            <h4>Emali: { contactos.email } </h4>
+            <h5>Estado: {conectado === false ? 'desconectado':'conectado'} </h5>
+            <button onClick={() => setConectado(!conectado)}>Cambiar estado</button>
         </div>
     );
 };
 
 
 Contactoscomponets.propTypes = {
-    contactos: PropTypes.instanceOf(Contactos),
-    nombre: PropTypes.string,
-    apellido: PropTypes.string,
-    email: PropTypes.string
-
+    contactos: PropTypes.instanceOf(Contactos)
 };
 
 
--- ejercicio sesion 1 2 y 3/src/container/lista_contactos.jsx
+++ ejercicio sesion 1 2 y 3/src/container/lista_contactos.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Contactoscomponets from '../components/contactos';
 import { Contactos } from '../Models/contactos.class';
 
-const ListaContactos = () => {
 
+const ListaContactos = ({estado}) => {
+
+  
     
     const defaultcontactos  = new Contactos('Juan','Peralta','jepr',false);
 
     return (
         <div>
-            <h1>Tus contactos</h1>
-            <Contactoscomponets contactos={defaultcontactos}></Contactoscomponets>
-            <button>Cambiar estado</button>
+          <h1>Tus contactos</h1>
+            <Contactoscomponets contactos={defaultcontactos}></Contactoscomponets>            
         </div>
     );
 }
