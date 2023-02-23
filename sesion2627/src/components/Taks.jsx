b0b1f5e52d55a475c780d2f25fd6bf19080d76d3
Your Name <you@example.com>
2023-02-23 17:26:10 -0400

    .

--- sesion2627/src/components/Taks.jsx
+++ sesion2627/src/components/Taks.jsx
@@ -6,8 +6,11 @@ const initialState = { taskList : [] };
 // Actions
 const ADD = 'ADD';
 const DELETE = 'DELETE';
-const COMPLETE = 'COMPLETE';
-const FILTER = 'FILTER';
+const STATUS = 'STATUS';
+const SHOW_COMPLETED = 'SHOW_COMPLETE';
+//const SHOW_INCOMPLETED = 'SHOW_INCOMPLETE';
+//const SHOW_ALL = 'SHOW_ALL'
+
 
 //Reducer to Handle Actions
 const reducer = (state, action) =>{
@@ -24,25 +27,25 @@ const reducer = (state, action) =>{
                 ]
             };
             case DELETE:{
-                const filteredTaksItem = state.taskList.filter((taskItem) => taskItem.id !== action.taskItemId);
-                return{taskList: filteredTaksItem};
+                const deleteTaksItem = state.taskList.filter((taskItem) => taskItem.id !== action.taskItemId);
+                return{taskList: deleteTaksItem};
             };
-            case COMPLETE:{
+            case STATUS:{
                 const updatedTaskList = state.taskList.map((taskItem) => taskItem.id === action.taskItemId ? {...taskItem, completed: !taskItem.completed} : taskItem);
                 return{taskList: updatedTaskList};
             };
-    
-        default:
-            return state;
+           /*case SHOW_COMPLETED: {
+           };*/
+            default:
+                return state;
     }
+    
 };
 
 const TaskListContext = createContext();
+export const Provider = ({ children }) => { 
 
-
-export const Provider = ({children}) => {
-    
-    const [state, dispatch] = useReducer(reducer,initialState);
+const [state, dispatch] = useReducer(reducer,initialState);
 
 const value = {
     taskList: state.taskList,
@@ -53,15 +56,24 @@ const value = {
     dispatch({ type: DELETE, taskItemId });
     },
     markAsCompleted: (taskItemId) => {
-    dispatch({ type: COMPLETE, taskItemId });
-    }
+    dispatch({ type: STATUS, taskItemId });
+    },
+    showCompleted: (taskItemId) => {
+        dispatch({ type: SHOW_COMPLETED, taskItemId});
+    },
+   /* showInCompleted: () => {
+        dispatch({type: SHOW_INCOMPLETED, })
+    },
+    showAll: () => {
+        dispatch({type: SHOW_ALL, })
+    }*/
 };
 
-return (
-    <TaskListContext.Provider value={value}>
-    {children}
-    </TaskListContext.Provider>
-);
+    return (
+        <TaskListContext.Provider value={value}>
+        {children}
+        </TaskListContext.Provider>
+    );
 }
 
 const Taks = () => {
@@ -69,17 +81,16 @@ const Taks = () => {
     const [inputValue, setInputValue] = useState('');
     const {addTaskItem} = useContext(TaskListContext);
     
-    const { taskList,  deleteTaskItem, markAsCompleted } = useContext(TaskListContext);
-    
+    const { taskList,  deleteTaskItem, markAsCompleted, showCompleted, /*showInCompleted, showAll*/ } = useContext(TaskListContext);
 return (    
     <div>
         <div className="card" style={{width: '50rem'}}>
         <div className="card-body">
             <div className='d-flex justify-content-between mb-4'>
-            <button className='btn-sm btn btn-light'>All Task</button>
             <h5 className="card-title h1">Task Table</h5>
+            <button  className='btn-sm btn btn-light'>All Task</button>
+            <button onClick={()=>{showCompleted(taskList.id)}} className='btn-sm btn-light btn'>completed</button>
             <button className='btn-sm btn btn-light'>Pending</button>
-            <button className='btn-sm btn-light btn'>completed</button>
             </div>
             <section className="card-text">
             <table className="table">
